bac0dba2b7c9df396869a976eb1b95ec742b9e4e
CEOJINSUNG <jinsung1048@gmail.com>
2022-02-05 17:45:32 +0900

    Update CafeinApplication.java

--- src/main/java/stop/covid/challenge/cafein/CafeinApplication.java
+++ src/main/java/stop/covid/challenge/cafein/CafeinApplication.java
@@ -2,7 +2,9 @@ package stop.covid.challenge.cafein;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class CafeinApplication {
 
