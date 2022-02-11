6e0ce8274f51910731308baf2e418c5c19c7d57a
CEOJINSUNG <jinsung1048@gmail.com>
2022-02-11 16:09:33 +0900

    0211_jinsung_각 도메인 별 jparepository 추가

---
+++ src/main/java/stop/covid/challenge/cafein/repository/CommentRepository.java
@@ -0,0 +1,8 @@
+package stop.covid.challenge.cafein.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<CommentRepository, Long> {
+}
---
+++ src/main/java/stop/covid/challenge/cafein/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package stop.covid.challenge.cafein.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import stop.covid.challenge.cafein.domain.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
